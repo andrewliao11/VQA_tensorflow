3784ad046d9b4c78f43008c109224f5a38103442
andrewliao11 <andrewliao11@gmail.com>
2016-03-01 00:18:26 +0800

    start answer_generator

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ def get_data():
 	data = json.load(data_file)
     for key in data.keys():
 	dataset[key] = data[key]
-    '''
+   
     # load image feature
     with h5py.File(image_feature_path,'r') as hf:
         tem = hf.get('images_train')
@@ -42,10 +42,52 @@ def get_data():
 	dataset['fv_im'] = np.divide(dataset['fv_im'],np.tile(tem,(1,4096)))
 	#local nm=torch.sqrt(torch.sum(torch.cmul(dataset['fv_im'],dataset['fv_im']),2))
         #dataset['fv_im']=torch.cdiv(dataset['fv_im'],torch.repeatTensor(nm,1,4096)):float()
-    '''
 
     return dataset
 
+class Answer_generator():
+    def __init__(self, dim_image, n_words, dim_hidden, batch_size, n_lstm_steps, drop_out_rate, bias_init_vector=None):
+        self.dim_image = dim_image
+        self.n_words = n_words
+        self.dim_hidden = dim_hidden
+        self.batch_size = batch_size
+        self.n_lstm_steps = n_lstm_steps
+        self.drop_out_rate = drop_out_rate
+
+	# LSTM cell
+	self.lstm1 = rnn_cell.LSTMCell(self.dim_hidden,self.dim_hidden,use_peepholes = True)
+        self.lstm1_dropout = rnn_cell.DropoutWrapper(self.lstm1,output_keep_prob=1 - self.drop_out_rate)
+        self.lstm2 = rnn_cell.LSTMCell(self.dim_hidden,2*self.dim_hidden,use_peepholes = True)
+        self.lstm2_dropout = rnn_cell.DropoutWrapper(self.lstm2,output_keep_prob=1 - self.drop_out_rate)
+
+	# word embedded
+	self.embed_word_W = tf.Variable(tf.random_uniform([dim_hidden, n_words], -0.1,0.1), name='embed_word_W')
+        self.embed_word_b = tf.Variable(bias_init_vector.astype(np.float32), name='embed_word_b')
+    def build_model(self):
+	# placeholder is for feeding data
+	image_feature = tf.placeholder(tf.float32, [self.batch_size, self.n_lstm_steps, self.dim_image])
+	# TODO
+	caption_emb = tf.placeholder(tf.int32, [self.batch_size, ??])       
+	question_emb = tf.placeholder(tf.int32, [self.batch_size, ??])
+
+	# LSTM state 
+	state1 = tf.zeros([self.batch_size, self.lstm1.state_size])
+        state2 = tf.zeros([self.batch_size, self.lstm2.state_size])
+        state1_return = tf.zeros([self.batch_size, self.lstm1.state_size])
+        state2_return = tf.zeros([self.batch_size, self.lstm2.state_size])
+
+    def build_generator(self):
+	# placeholder si for feeding data
+	image_feature = tf.placeholder(tf.float32, [1, self.n_lstm_steps, self.dim_image])
+	# TODO
+        caption_emb = tf.placeholder(tf.int32, [1, ??])
+        question_emb = tf.placeholder(tf.int32, [1, ??])
+
+	# LSTM state
+        state1 = tf.zeros([1, self.lstm1.state_size])
+        state2 = tf.zeros([1, self.lstm2.state_size])
+        state1_return = tf.zeros([1, self.lstm1.state_size])
+        state2_return = tf.zeros([1, self.lstm2.state_size])
 
 def train():
 
