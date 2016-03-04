52e1d37ed7f4402713ed57833e703b109dc69970
andrewliao11 <andrewliao11@gmail.com>
2016-03-04 16:59:56 +0800

    finish answer

--- train.py
+++ train.py
@@ -18,10 +18,13 @@ image_feature_path = '/home/andrewliao11/VQA_LSTM_CNN/data_img.h5'
 
 ## Some args
 normalize = True
+max_words_q = 30
+num_answer = 1000
+
 def get_data():
 
     dataset = {}
-
+    train_data = {}
     # load json file
     print('Loading json file...')
     with open(json_data_path) as data_file:
@@ -34,56 +37,47 @@ def get_data():
     with h5py.File(image_feature_path,'r') as hf:
 	# -----0~82459------
         tem = hf.get('images_train')
-        dataset['fv_im'] = np.array(tem)
+	img_feature = np.array(tem)
+        #dataset['fv_im'] = np.array(tem)
     # load h5 file
     print('Loading h5 file...')
     with h5py.File(h5_data_path,'r') as hf:
 	# total number of training data is 215375
 	# question is (26, )
 	tem = hf.get('ques_train')
-	dataset['question'] = np.array(tem)
+	train_data['question'] = np.array(tem)
 	# max length is 23
 	tem = hf.get('ques_length_train')
-        dataset['lengths_q'] = np.array(tem)
+        train_data['length_q'] = np.array(tem)
 	# total 82460 img 
 	# -----1~82460-----
 	tem = hf.get('img_pos_train')
-        dataset['img_list'] = np.array(tem)
+        train_data['img_list'] = np.array(tem)
 	# answer is 1~1000
 	tem = hf.get('answers')
-        dataset['answers'] = np.array(tem)
+        train_data['answers'] = np.array(tem)
    
-    #print(np.multiply(dataset['fv_im'],dataset['fv_im']).shape)
     print('Normalizing image feature')
     if normalize:
-	tem =  np.sqrt(np.sum(np.multiply(dataset['fv_im'],dataset['fv_im'])))
-	dataset['fv_im'] = np.divide(dataset['fv_im'],np.tile(tem,(1,4096)))
-	#local nm=torch.sqrt(torch.sum(torch.cmul(dataset['fv_im'],dataset['fv_im']),2))
-        #dataset['fv_im']=torch.cdiv(dataset['fv_im'],torch.repeatTensor(nm,1,4096)):float()
+	tem =  np.sqrt(np.sum(np.multiply(img_feature, img_feature)))
+	img_feature = np.divide(img_feature, np.tile(tem,(1,4096)))
 
-    return dataset
+    return dataset, img_feature, train_data
+
 
 class Answer_Generator():
-    def __init__(self, dim_image, n_words_q, n_words_c, dim_hidden, batch_size, n_lstm_steps, drop_out_rate, bias_init_vector=None):
-        self.dim_image = dim_image
+    def __init__(self, dim_image, n_words_q, dim_hidden, batch_size, drop_out_rate, bias_init_vector=None):
+        print('Initialize the model')
+	self.dim_image = dim_image
         self.n_words_q = n_words_q
-	self.n_words_c = n_words_c
         self.dim_hidden = dim_hidden
         self.batch_size = batch_size
-        self.n_lstm_steps = n_lstm_steps
         self.drop_out_rate = drop_out_rate
 
 	# 2 layers LSTM cell
 	self.lstm = rnn_cell.BasicLSTMCell(self.dim_hidden,self.dim_hidden)
 	self.lstm_dropout = rnn_cell.DropoutWrapper(self.lstm,output_keep_prob=1 - self.drop_out_rate)
 	self.stacked_lstm = rnn_cell.MultiRNNCell([self.lstm_dropout] * 2)
-
-	'''
-	self.lstm1 = rnn_cell.LSTMCell(self.dim_hidden,self.dim_hidden,use_peepholes = True)
-        self.lstm1_dropout = rnn_cell.DropoutWrapper(self.lstm1,output_keep_prob=1 - self.drop_out_rate)
-        self.lstm2 = rnn_cell.LSTMCell(self.dim_hidden,2*self.dim_hidden,use_peepholes = True)
-        self.lstm2_dropout = rnn_cell.DropoutWrapper(self.lstm2,output_keep_prob=1 - self.drop_out_rate)
-	'''
 	
 	# image feature embedded
 	self.embed_image_W = tf.Variable(tf.random_uniform([dim_image, dim_hidden], -0.1,0.1), name='embed_image_W')
@@ -94,170 +88,151 @@ class Answer_Generator():
 	
 	# embed the word into lower space
 	with tf.device("/cpu:0"):
-            self.question_emb_W = tf.Variable(tf.random_uniform([n_words_q, dim_hidden], -0.1, 0.1), name='question_emb_Q')
-	    self.question_emb_b = tf.zeros([dim_hidden], name='question_emb_b')
-	#with tf.device("/cpu:0"):
-        #    self.caption_emb_matrix = tf.Variable(tf.random_uniform([n_words_c, dim_hidden], -0.1, 0.1), name='caption_emb_matrix')
+            self.question_emb_W = tf.Variable(tf.random_uniform([n_words_q, dim_hidden], -0.1, 0.1), name='question_emb_W')
 
-	# TODO:embed loser space into word
-	'''
-	# question embedded
-	self.embed_question_W = tf.Variable(tf.random_uniform([dim_hidden, n_words_q], -0.1,0.1), name='embed_question_W')
+	# embed lower space into answer
+	self.answer_emb_W = tf.Variable(tf.random_uniform([dim_hidden, num_answer], -0.1, 0.1), name='answer_emb_W')	
 	if bias_init_vector is not None:
-            self.embed_word_b = tf.Variable(bias_init_vector.astype(np.float32), name='embed_question_b')
+            self.answer_emb_b = tf.Variable(bias_init_vector.astype(np.float32), name='answer_emb_b')
         else:
-            self.embed_word_b = tf.Variable(tf.zeros([n_words]), name='embed_question_b')
+            self.answer_emb_b = tf.Variable(tf.zeros([num_answer]), name='answer_emb_b')	
 
-	#caption embedded
-	with tf.device("/cpu:0"):
-            self.caption_emb = tf.Variable(tf.random_uniform([n_words_c, dim_hidden], -0.1, 0.1), name='caption_emb')
-	self.embed_caption_W = tf.Variable(tf.random_uniform([dim_hidden, n_words_c], -0.1,0.1), name='embed_caption_W')
-	if bias_init_vector is not None:
-            self.embed_word_b = tf.Variable(bias_init_vector.astype(np.float32), name='embed_word_b')
-        else:
-            self.embed_word_b = tf.Variable(tf.zeros([n_words]), name='embed_word_b')
-	'''
+	# record the final word index
+	# question_mask = tf.Variable(tf.zeros([self.batch_size]),name='question_mask')
+	
 
     def build_model(self):
+	
+	print('building model')
 	# placeholder is for feeding data
-	image_feature = tf.placeholder(tf.float32, [self.batch_size, self.dim_image])  # (batch_size, dim_image)
-	question = tf.placeholder(tf.float32, [self.batch_size, self.n_words_q])  # (batch_size, n_words)
-	question_len = tf.placeholder(tf.float32, [self.batch_size])  # (batch_size, )
-	answer = tf.placeholder(tf.float32, [self.batch_size])	# (batch_size, )
-	# TODO  
-	# caption_emb = tf.placeholder(tf.int32, [self.batch_size, ??])
+	image = tf.placeholder(tf.float32, [self.batch_size, self.dim_image])  # (batch_size, dim_image)
+	question = tf.placeholder(tf.int32, [self.batch_size, max_words_q])
+	question_length = tf.placeholder(tf.int32, [self.batch_size])
+	label = tf.placeholder(tf.int32, [self.batch_size])
+	#question_mask = tf.placeholder(tf.float32, [self.batch_size])	
 
-	image_emb = tf.nn.xw_plus_b(image_feature, self.embed_image_W, self.embed_image_b) # (batch_size, dim_hidden)
-	image_emb = tf.nn.dropout(image_emb, self.drop_out_rate)
-	image_emb = tf.tanh(image_emb)
-	question_emb = tf.nn.xw_plus_b(question, self.embed_question_W, self.embed_question_b) # (batch_size, dim_hidden)
-	question_emb = tf.nn.xw_plus_b(question_emb, self.drop_out_rate)
-	question_emb = tf.tanh(question_emb)
-	# TODO
-	# caption_emb = tf.nn.xw_plus_b(caption, self.embed_caption_W, self.embed_caption_b) # (batch_size, dim_hidden)
-        # caption_emb = tf.nn.xw_plus_b(caption_emb, self.drop_out_rate)
-        # caption_emb = tf.tanh(caption_emb)
+	# [image] embed image feature to dim_hidden
+        image_emb = tf.nn.xw_plus_b(image, self.embed_image_W, self.embed_image_b) # (batch_size, dim_hidden)
+        image_emb = tf.nn.dropout(image_emb, self.drop_out_rate)
+        image_emb = tf.tanh(image_emb)
 
-	# initialize stzcked-LSTM state 
-	state = tf.zeros([self.batch_size, self.stacked_lstm.state_size])
+	# [answer] ground truth
+        labels = tf.expand_dims(label, 1) # b x 1
+        indices = tf.expand_dims(tf.range(0, self.batch_size, 1), 1) # b x 1
+        concated = tf.concat(1, [indices, labels]) # b x 2
+        answer = tf.sparse_to_dense(concated, tf.pack([self.batch_size, num_answer]), 1.0, 0.0) # (batch_size, num_answer)
+
+	# [question_mask] 
+	question_mask = tf.to_float(question_length) # (batch_size, )
+        #question_mask = tf.expand_dims(question_mask,2)
+        question_mask = tf.expand_dims(question_mask,1)
+        question_mask = tf.tile(question_mask,tf.constant([1,1,max_words_q]))
+        question_mask = tf.transpose(question_mask, perm=[1, 0, 2]
+
 	'''
-	state1 = tf.zeros([self.batch_size, self.lstm1.state_size])
-        state2 = tf.zeros([self.batch_size, self.lstm2.state_size])
-        state1_return = tf.zeros([self.batch_size, self.lstm1.state_size])
-        state2_return = tf.zeros([self.batch_size, self.lstm2.state_size])
+	# one hot vector
+	labels_q = tf.expand_dims(question_length, 1) # b x 1
+        indices_q = tf.expand_dims(tf.range(0, self.batch_size, 1), 1) # b x 1
+        concated_q = tf.concat(1, [indices_q, labels_q]) # b x 2
+        question_mask = tf.sparse_to_dense(concated_q, tf.pack([self.batch_size, max_words_q]), 1.0, 0.0) # (batch_size, max_words_q)
 	'''
-
 	probs = []
         loss = 0.0
-        #state1_temp=[]
-        #state2_temp=[]
 
-	# TODO: calculate max length of caption and question
-	for j in range(30): 
+        state = tf.zeros([self.batch_size, max_words_q]) 
+	output = tf.zeros([self.batch_size, max_words_q])
+
+	states = []
+	outputs = []
+	for j in range(max_words_q): 
             if j == 0:
                 question_emb = tf.zeros([self.batch_size, self.dim_hidden])
-		#caption_emb = tf.zer0s([self.batch_size, self.dim_hidden])
-	    '''
             else:
-                # cuttent question/caption embed => ground truth
                 with tf.device("/cpu:0"):
-		    # TODO: where to feed the data??
-		    #current_embed = tf.nn.embedding_lookup(self.Wemb, caption[:,j-1])
-                    question_emb = tf.nn.embedding_lookup(self.question_emb_matrix, ?)
-		    caption_emb = tf.nn.embedding_loofup(self.caption_emb_matrix, ?)
-	    '''
-	    if j > 0:
-		tf.get_variable_scope().reuse_variables()
+		    tf.get_variable_scope().reuse_variables()
+                    question_emb = tf.nn.embedding_lookup(self.question_emb_W, question[:,j-1])
 	    output, state = self.stacked_lstm(tf.concat(1,[image_emb, question_emb]), state)
+	    # record the state an output
+	    states.append(state)
+	    outputs.append(output)
 	
-	    
-	    '''
-            with tf.variable_scope("LSTM1"):
-                output1, state1 = self.lstm1_dropout( padding, state1 )
+	
+	# predict   
+	outputs = tf.pack(outputs) # (batch_size, max_words_q, dim_hidden)
+	output_final = tf.reduce_sum(tf.mul(outputs, question_mask), 1) # (batch_size, )
+	answer_pred = tf.nn.xw_plus_b(output_final, self.answer_emb_W, self.answer_emb_b) # (batch_size, num_answer)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(answer_pred, answer) # (batch_size, )
+        # cross_entropy = cross_entropy * caption_mask[:,j]
+	loss = tf.reduce_sum(cross_entropy)
+	loss = loss/self.batch_size
 
-            with tf.variable_scope("LSTM2"):
-                output2, state2 = self.lstm2_dropout( tf.concat(1,[current_embed, output1]), state2 ) 
-	    '''
-    '''
-    def build_generator(self):
-	# placeholder is for feeding data
-	image_feature_emb = tf.placeholder(tf.float32, [1, self.n_lstm_steps, self.dim_image])
-	# TODO
-        #caption_emb = tf.placeholder(tf.int32, [1, ??])
-        #question_emb = tf.placeholder(tf.int32, [1, ??])
+	return loss
 
-	# LSTM state
-        state1 = tf.zeros([1, self.lstm1.state_size])
-        state2 = tf.zeros([1, self.lstm2.state_size])
-        state1_return = tf.zeros([1, self.lstm1.state_size])
-        state2_return = tf.zeros([1, self.lstm2.state_size])
-    '''
 ## Train Parameter
 dim_image = 4096
 dim_hidden = 1024
-#embedded_size_question = 1024
-#embedded_size_image = 1024
-#embedded_size_caption = 1024
-# TODO: modify it
-n_frame_step = 110
-
 n_epochs = 300
 batch_size = 10
 learning_rate = 0.0001 #0.001
-'''
-def preProBuildWordVocab(sentence_iterator, word_count_threshold=5): # borrowed this function from NeuralTalk
-    print 'preprocessing word counts and creating vocab based on word count threshold %d' % (word_count_threshold, )
-    word_counts = {}
-    nsents = 0
-    for sent in sentence_iterator:
-        nsents += 1
-        for w in sent.lower().split(' '):
-           word_counts[w] = word_counts.get(w, 0) + 1
 
-    vocab = [w for w in word_counts if word_counts[w] >= word_count_threshold]
-    print 'filtered words from %d to %d' % (len(word_counts), len(vocab))
-
-    ixtoword = {}
-    ixtoword[0] = '.'  # period at the end of the sentence. make first dimension be end token
-    wordtoix = {}
-    wordtoix['#START#'] = 0 # make first vector be the start token
-    ix = 1
-    for w in vocab:
-        wordtoix[w] = ix
-        ixtoword[ix] = w
-        ix += 1
-
-    word_counts['.'] = nsents
-    bias_init_vector = np.array([1.0*word_counts[ixtoword[i]] for i in ixtoword])
-    bias_init_vector /= np.sum(bias_init_vector) # normalize to frequencies
-    bias_init_vector = np.log(bias_init_vector)
-    bias_init_vector -= np.max(bias_init_vector) # shift to nice numeric range
-    return wordtoix, ixtoword, bias_init_vector
-'''
 def train():
 
     print('Start to load data!')
-    dataset = get_data()
-    
+    dataset, img_feature, train_data = get_data()
+    num_train = train_data['question'].shape[0]
     # count question and caption vocabulary size
     vocabulary_size_q = len(dataset['ix_to_word'].keys())
-    print(vocabulary_size_q)
-    
     
     model = Answer_Generator(
             dim_image = dim_image,
             n_words_q = vocabulary_size_q,
-	    # TODO: calcualte caption vocab size
-	    n_words_c = 1000,
 	    dim_hidden = dim_hidden,
-            #embedded_size_question = embedded_size_question,
-	    #embedded_size_image = embedded_size_image,
-	    #embedded_size_caption = embedded_size_caption,
             batch_size = batch_size,
-            n_lstm_steps = n_frame_step,
             drop_out_rate = 0.5,
             bias_init_vector = None)
     
+    tf_loss = model.build_model()
+    '''
+    sess = tf.InteractiveSession()
+    writer = tf.train.SummaryWriter('/tmp/tf_log', sess.graph_def)
+    saver = tf.train.Saver(max_to_keep=100)
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(tf_loss)
+    tf.initialize_all_variables().run()
+    
+    tStart_total = time.time()
+    for epoch in range(n_epochs):
+	# shuffle the training data
+	index = np.arange(num_train)
+        np.random.shuffle(index)
+        train_data['question'] = train_data['question'][index,:]
+	train_data['length_q'] = train_data['length_q'][index,:]
+	train_data['answers'] = train_data['answers'][index,:]
+        train_data['img_list'] = train_data['img_list'][index,:]
+
+        tStart_epoch = time.time()
+        loss_epoch = np.zeros(len(train_data))
+	for current_batch_file_idx in xrange(num_train):
+
+            tStart = time.time()
+            #current_batch = h5py.File(train_data[current_batch_file_idx])
+            current_img = np.zeros((batch_size, dim_image))
+            current_question = np.zeros((batch_size, max_words_q))
+            current_question_length = np.zeros((batch_size))
+            current_label = np.zeros((batch_size))
+            # set input data into current_*
+            # one batch at a time
+            for ind in xrange(batch_size):
+                current_feats[ind,:,:] = current_batch['data'][:,ind,:]
+                idx = np.where(current_batch['label'][:,ind] != -1)[0]
+                if len(idx) == 0:
+                        continue
+                idy = np.where(current_batch['label'][:,ind] == 1)[0]
+                if len(idy) == 0:
+                        continue
+                current_HLness[ind,idx] = current_batch['label'][idx,ind]
+                current_HLness_masks[ind,idx] = 1
+                current_video_masks[ind,idy[-1]] = 1
+    '''    	
 
 if __name__ == '__main__':
     train()
