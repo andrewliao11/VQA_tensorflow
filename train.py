3d373cf9d3952dd858687bbfa670a537041fce27
andrewliao11 <andrewliao11@gmail.com>
2016-03-08 23:45:25 +0800

    complete training

--- train.py
+++ train.py
@@ -114,6 +114,7 @@ class Answer_Generator():
 	image = tf.placeholder(tf.float32, [self.batch_size, self.dim_image])  # (batch_size, dim_image)
 	question = tf.placeholder(tf.int32, [self.batch_size, max_words_q])
 	question_length = tf.placeholder(tf.int32, [self.batch_size])
+	question_mask = tf.placeholder(tf.int32, [max_words_q, self.batch_size, 2*self.dim_hidden])
 	label = tf.placeholder(tf.int32, [self.batch_size]) # (batch_size, )
 
 	# [image] embed image feature to dim_hidden
@@ -126,13 +127,27 @@ class Answer_Generator():
         indices = tf.expand_dims(tf.range(0, self.batch_size, 1), 1) # (batch_size, 1)
         concated = tf.concat(1, [indices, labels]) # (batch_size, 2)
         answer = tf.sparse_to_dense(concated, tf.pack([self.batch_size, num_answer]), 1.0, 0.0) # (batch_size, num_answer)
-
+	
+	'''
+	# [question_mask] 
+	question_length_exp = tf.expand_dims(question_length, 1) # b x 1
+        indices_q = tf.expand_dims(tf.range(0, self.batch_size, 1), 1) # b x 1
+        concated_tem = tf.concat(1, [indices_q, question_length_exp]) # b x 2
+	question_length_exp_exp = tf.expand_dims(concated_tem, 2) # (batch_size, 2, 1)
+	indices_2dim_hidden_exp = tf.expand_dims(tf.range(0, 2*dim_hidden, 1), 0) # (1, 2*dim_hidden)
+	indices_2dim_hidden_exp_exp = tf.expand_dims(indices_2dim_hidden_exp, 0) # (1, 1, 2*dim_hidden)
+	concated_q = tf.add(question_length_exp_exp,indices_2dim_hidden_exp_exp) # (batch_size, 2, 2*dim_hidden)
+	#concate = tf.concat(2, [question_length_exp_exp, indices_2dim_hidden_exp_exp]) # (batch_size, 2, 2*dim_hidden)
+        question_mask = tf.sparse_to_dense(concated_q, tf.pack([self.batch_size, max_words_q, 2*dim_hidden]), 1.0, 0.0) # (batch_size, max_words_q, 2*dim_hidden)
+	
 	# [question_mask] 
 	labels_q = tf.expand_dims(question_length, 1) # b x 1
         indices_q = tf.expand_dims(tf.range(0, self.batch_size, 1), 1) # b x 1
         concated_q = tf.concat(1, [indices_q, labels_q]) # b x 2
         question_mask = tf.sparse_to_dense(concated_q, tf.pack([self.batch_size, max_words_q]), 1.0, 0.0) # (batch_size, max_words_q)
-	
+	#question_mask_tem_tem = tf.expand_dims(question_mask_tem, 2) # (batch_size, max_words_q,  1)
+	#question_mask_ = tf.tile(question_mask_tem_tem, 2*dim_hidden) # (batch_size, max_words_q, 2*dim_hidden)
+	'''
 	probs = []
         loss = 0.0
 
@@ -154,25 +169,103 @@ class Answer_Generator():
 	    with tf.variable_scope("LSTM1"):
                 output1, state1 = self.lstm1_dropout(tf.concat(1,[image_emb, question_emb]), state1 )
             with tf.variable_scope("LSTM2"):
-                output, state2 = self.lstm2_dropout(output1, state2 )
+                output2, state2 = self.lstm2_dropout(output1, state2 )
 	    #output, state = self.stacked_lstm(image_emb, state)
 	    #output, state = self.stacked_lstm(tf.concat(1,[image_emb, question_emb]), state)
+
 	    # record the state an output
 	    states.append(state2)
-	    outputs.append(output)
-	
+	    outputs.append(output2)
+	    	
 	
 	# predict   
 	# pack -> convert input into an array
-	outputs = tf.pack(outputs) # (max_words_q, batch_size, 2*dim_hidden)
-	#outputs = outputs[:,range(self.batch_size),:]
-	output_final = tf.reduce_sum(tf.mul(outputs, question_mask), 1) # (batch_size, 2*dim_hidden)
+	states = tf.pack(outputs) # (max_words_q, batch_size, 4*dim_hidden)
+	#states = tf.gather(states, tf.range(0, max_words_q, 1)) # (max_words_q, batch_size, 2*dim_hidden)
+	print (states)
+	'''
+	tem = []
+	for i in range(self.batch_size):
+	    tem.append(outputs[:,i,:])
+	tem = tf.pack(tem)  # (batch_size, max_words_q, 2*dim_hidden)
+	'''
+	#for i in range(self.batch_size):
+	'''
+	states = tf.gather(states, question_length)
+	print (states)
+	states = tf.gather(range(10), states)
+	print (states)
+	'''
+	#output_final = states[question_length, tf.range(0, self.batch_size, 1), :]
+	#output_final = tf.pack()
+	output_final = tf.reduce_sum(tf.mul(states, tf.to_float(question_mask)), 0) # (batch_size, 2*dim_hidden)
+	print (output_final)
+	#output_final = tf.reduce_sum(output_final, 1) # (batch_size)
+	#print (output_final)
 	answer_pred = tf.nn.xw_plus_b(output_final, self.answer_emb_W, self.answer_emb_b) # (batch_size, num_answer)
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(answer_pred, answer) # (batch_size, )
 	loss = tf.reduce_sum(cross_entropy)
 	loss = loss/self.batch_size
 
-	return loss, image, question, question_length, label
+	return loss, image, question, question_mask, question_length, label
+    '''
+    def build_generator(self):
+	
+        # placeholder is for feeding data
+        image = tf.placeholder(tf.float32, [1, self.dim_image])  # (1, dim_image)
+        question = tf.placeholder(tf.int32, [1, max_words_q])
+        question_length = tf.placeholder(tf.int32, [1])
+        question_mask = tf.placeholder(tf.int32, [1, max_words_q, self.dim_image])
+
+        # [image] embed image feature to dim_hidden
+        image_emb = tf.nn.xw_plus_b(image, self.embed_image_W, self.embed_image_b) # (1, dim_hidden)
+        image_emb = tf.nn.dropout(image_emb, self.drop_out_rate)
+        image_emb = tf.tanh(image_emb)
+
+	# [question_mask]
+        labels_q = tf.expand_dims(question_length, 1) # (1, 1)
+        indices_q = tf.expand_dims(tf.range(0, 1, 1), 1) # (1, 1)
+        concated_q = tf.concat(1, [indices_q, labels_q]) # (1, 2)
+        question_mask = tf.sparse_to_dense(concated_q, tf.pack([self.batch_size, max_words_q]), 1.0, 0.0) # (1, max_words_q)
+        question_mask = tf.expand_dims(question_mask, 2) # (1, max_words_q,  1)
+        question_mask = tf.tile(question_mask, 2*dim_hidden) # (1, max_words_q, 2*dim_hidden)
+
+        state1 = tf.zeros([1, self.lstm1.state_size])
+        state2 = tf.zeros([1, self.lstm2.state_size])
+
+	#generated_answer = []
+	outputs = []
+	states = []
+
+	for j in range(max_words_q):
+	    tf.get_variable_scope().reuse_variables()
+            if j == 0:
+                question_emb = tf.zeros([1, self.dim_hidden])
+            else:
+                with tf.device("/cpu:0"):
+                    question_emb = tf.nn.embedding_lookup(self.question_emb_W, question[:,j-1]) #(dim_hidden, )
+		    question_emb = tf.expand_dims(question_emb, 0) # (1, dim_hidden)
+            with tf.variable_scope("LSTM1"):
+                output1, state1 = self.lstm1_dropout(tf.concat(1,[image_emb, question_emb]), state1 )
+            with tf.variable_scope("LSTM2"):
+                output2, state2 = self.lstm2_dropout(output1, state2 )
+
+            # record the state an output
+            states.append(state2)
+            outputs.append(output2)
+	
+	# predict
+        outputs = tf.pack(outputs) # (max_words_q, 1, 2*dim_hidden)
+        tem = outputs[:,1,:] # (max_words_q, 2*dim_hidden)
+	tem = tf.expands_dims(tem, 0) # (1, max_words_q, 2*dim_hidden)
+        output_final = tf.reduce_sum(tf.mul(tem, question_mask), 1) # (1, 2*dim_hidden)
+        answer_pred = tf.nn.xw_plus_b(output_final, self.answer_emb_W, self.answer_emb_b) # (1, num_answer)
+
+        max_prob_index = tf.argmax(answers_pred, 1)[0]
+        generated_answer = max_prob_index
+    
+        return generated_answer, image, question, question_length
+    '''
 
 ## Train Parameter
 dim_image = 4096
@@ -197,7 +290,7 @@ def train():
             drop_out_rate = 0.5,
             bias_init_vector = None)
     
-    tf_loss, tf_image, tf_question, tf_question_length, tf_label = model.build_model()
+    tf_loss, tf_image, tf_question, tf_question_mask, tf_question_length, tf_label = model.build_model()
     
     sess = tf.InteractiveSession()
     writer = tf.train.SummaryWriter('/home/andrewliao11/Word/VQA_challenge/tmp/tf_log', sess.graph_def)
@@ -214,7 +307,7 @@ def train():
 	train_data['length_q'] = train_data['length_q'][index] # (num_train, )
 	train_data['answers'] = train_data['answers'][index]
         train_data['img_list'] = train_data['img_list'][index]
-
+	
         tStart_epoch = time.time()
         loss_epoch = np.zeros(num_train)
 	num_batch = num_train/batch_size + 1
@@ -224,17 +317,16 @@ def train():
             tStart = time.time()
             # set data into current*
 	    current_question = train_data['question'][current_batch_file_idx,:]
+	    #current_wuestion_mask = question_mask[:,current_batch_file_idx,:]
             current_length_q = train_data['length_q'][current_batch_file_idx]
             current_answers = train_data['answers'][current_batch_file_idx]
             current_img_list = train_data['img_list'][current_batch_file_idx]
-	    # init the para
+	   
 	    current_img = np.zeros((batch_size, dim_image))
-	    #current_img_idx = np.zeros((batch_size))
-            #current_question = np.zeros((batch_size, max_words_q))
-            #current_question_length = np.zeros((batch_size))
-            #current_label = np.zeros((batch_size))
-
 	    current_img = img_feature[current_img_list,:] # (batch_size, dim_image)
+	   
+	    current_question_mask = np.zeros([max_words_q, batch_size, 2*dim_hidden])
+    	    current_question_mask[current_length_q, range(batch_size), :] = 1 #(max_words_q, batch_size, 2*dim_hidden)
 	    '''
             # one batch at a time
             #for idx_batch in xrange(batch_size):
@@ -272,6 +364,7 @@ def train():
                         tf_image: current_img,
                         tf_question: current_question,
                         tf_question_length: current_length_q,
+			tf_question_mask: current_question_mask,
                         tf_label: current_answers
                         })
 	    loss_epoch[current_batch_file_idx] = loss
@@ -279,6 +372,64 @@ def train():
             print ("Epoch:", epoch, " Batch:", current_batch_file_idx, " Loss:", loss)
             print ("Time Cost:", round(tStop - tStart,2), "s")
 
+'''
+def test(model_path='models/model-900', video_feat_path=video_feat_path):
+
+
+    print('Start to load data!')
+    dataset, img_feature, train_data = get_data()
+    num_test = train_data['question'].shape[0]
+    # count question and caption vocabulary size
+    vocabulary_size_q = len(dataset['ix_to_word'].keys())
+
+    model = Answer_Generator(
+            dim_image = dim_image,
+            n_words_q = vocabulary_size_q,
+            dim_hidden = dim_hidden,
+            batch_size = batch_size,
+            drop_out_rate = 0,
+            bias_init_vector = None)
+	
+    generated_answer_tf, image_tf, question_tf, question_length_tf = model.build_generator()
+    sess = tf.InteractiveSession()
+
+    saver = tf.train.Saver()
+    saver.restore(sess, model_path)
+
+    # TODO
+    mp = []
+    pred_sent = []
+    gt_sent = []
+    HLness = []
+    for idx, video_feat_path in enumerate(test_data):
+        print video_feat_path
+        test_data_batch = h5py.File(video_feat_path)
+        gt_captions = test_data_batch['title']
+        for xxx in xrange(test_data_batch['label'].shape[1]):
+                video_feat = np.zeros((1, n_frame_step, dim_image))
+                video_mask = np.zeros((1, n_frame_step))
+                video_feat[0,:,:] = test_data_batch['data'][:,xxx,:]
+                idx = np.where(test_data_batch['label'][:,xxx] != -1)[0]
+                video_mask[0,idx] = 1
+
+                #generated_word_index = sess.run(caption_tf, feed_dict={video_tf:video_feat, video_mask_tf:video_mask})
+                generated_HL_index = sess.run(HLness_tf, feed_dict={video_tf:video_feat, video_mask_tf:video_mask})
+                generated_word_index = sess.run(caption_tf, feed_dict={video_tf:video_feat, video_mask_tf:video_mask})
+                state1, state1_tmp = sess.run([state1_tf,state2_tf], feed_dict={video_tf:video_feat, video_mask_tf:video_mask})
+                #print state1[0,:100]
+                #print generated_word_index
+                #HL_words = sess.run(HL_words_tf, feed_dict={video_tf:video_feat, video_mask_tf:video_mask})
+                #generated_HL_index = HL_words[0]
+                #generated_word_index = np.array(generated_HL_index)[n_frame_step:]
+                #print HL_words
+                #print generated_word_index, generated_HL_index
+                probs_val = sess.run(probs_tf, feed_dict={video_tf:video_feat})
+                embed_val = sess.run(last_embed_tf, feed_dict={video_tf:video_feat})
+'''
+
+
 
 if __name__ == '__main__':
-    train()
+    with tf.device('/gpu:'+str(6)):
+        train()
+   #train()
